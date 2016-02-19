cdb83cdcd8ab33d35b5a50696b7889af0f3a974c
lesleycl <lesleychai518@gmail.com>
2016-02-18 21:06:46 -0500

    Github App Demo

---
+++ GithubDemo/RepoCell.swift
@@ -0,0 +1,17 @@
+//
+//  RepoCell.swift
+//  GithubDemo
+//
+//  Created by lesleychai on 2/18/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+
+class RepoCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
